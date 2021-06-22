a9b81642217cee311759433f203343f3ffaf5426
Allan Hipolito <allanhipolito66@gmail.com>
2021-06-22 08:55:10 -0300

    Primeira Aula NLW

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {SignIn} from './src/screens/SignIn';
+
+export default function App() {
+  return (
+   <SignIn/>
+  );
+}
+
